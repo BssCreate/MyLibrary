65ce16f43d9f5d09cc44bb61d55b0663e155fdba
BssCreate <bed_random@mail.ru>
2025-03-19 21:57:19 +0700

    Create googleSheets.js

---
+++ googleSheets.js
@@ -0,0 +1,36 @@
+const { GoogleSpreadsheet } = require('google-spreadsheet');
+const { JWT } = require('google-auth-library');
+const fs = require('fs');
+
+// Загружаем ключи из JSON-файла
+const creds = JSON.parse(fs.readFileSync('service-account.json'));
+
+// ID таблицы (из URL Google Sheets)
+const SHEET_ID = 'YOUR_SHEET_ID';
+
+// Функция для получения данных
+async function fetchUsersData() {
+    const doc = new GoogleSpreadsheet(SHEET_ID);
+    
+    const auth = new JWT({
+        email: creds.client_email,
+        key: creds.private_key,
+        scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly'],
+    });
+
+    await doc.useServiceAccountAuth(auth);
+    await doc.loadInfo();
+    
+    const sheet = doc.sheetsByIndex[0]; // Первый лист
+    const rows = await sheet.getRows(); // Получаем все строки
+
+    let users = {};
+    rows.forEach(row => {
+        users[row['Номер телефона'].replace(/\D/g, '')] = row['Пароль'];
+    });
+
+    return users;
+}
+
+// Экспортируем функцию
+module.exports = { fetchUsersData };
